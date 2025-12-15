624ee298501bf1fe32d0af5e5a5bf55ddf0d29f7
noah <baumgartner.noah@outlook.com>
2025-12-15 13:10:29 +0100

    started with footer implementation

---
+++ src/lib/components/Copyright.svelte
@@ -0,0 +1,5 @@
+<div class="border-b border-neutral-300 px-4 sm:px-10 lg:px-16">
+    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-300 flex items-center justify-center relative">
+        <span class="text-sm text-neutral-700 py-7 h-auto">© 2025 Noah Baumgartner</span>
+    </div>
+</div>
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,15 +4,16 @@
 </script>
 
 <div class="border-b border-neutral-300 px-4 sm:px-10 lg:px-16">
-    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-300 min-h-92 flex items-center justify-center relative">
+    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-300 min-h-92 flex items-center justify-center relative bg-linear-150 from-neutral-950 from-50% via-neutral-900 via-70% to-neutral-950 to-90%">
         <ParagraphNumber number="<>"  />
         <ComponentName name="Footer.svelte" position="bottom-left" />
-        <div class="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-16 justify-between p-10">
-            Coming soon...
+        <div class="flex flex-col sm:flex-row w-full">
+            <div class="w-2/3 px-10">
+                <span class="text-xl font-semibold font-[Google_Sans] text-neutral-100">Bis bald.</span>
+            </div>
+            <div class="w-1/3 px-10">
+                <span class="text-neutral-400">© 2024 Noah Baumgartner</span>
+            </div>
         </div>
-    </div>
-</div>
-<div class="px-4 sm:px-10 lg:px-16">
-    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-300 min-h-4 flex items-center justify-center relative">
     </div>
 </div>
