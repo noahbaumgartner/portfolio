b2d66a4cca1440f66d4f0e9c647f544ea642a1bb
noah <baumgartner.noah@outlook.com>
2025-12-17 06:42:30 +0100

    changed herosection texts

--- src/lib/components/sections/HeroSection.svelte
+++ src/lib/components/sections/HeroSection.svelte
@@ -8,9 +8,9 @@
     <div class="py-4 mx-auto max-w-7xl border-l border-r px-6 border-neutral-300 min-h-96 flex items-center justify-center grid-background relative">
         <ComponentName name="HeroSection.svelte" position="bottom-left" />
         <div class="flex flex-col space-y-2 items-center">
-            <Heading1>Software Entwickler</Heading1>
+            <Heading1>Fullstack Developer & Student</Heading1>
             <span class="text-center text-lg max-w-xl">
-                Ich entwickle Software bei <Link href="https://buildagil.com" color="#006666">buildagil</Link> und Open Source für <Link href="https://stack.swiss" color="#f13608">STACK</Link>. Zudem arbeite ich auch an privaten <Link href="/projects">Projekten</Link>.
+                Ich entwickle Software bei <Link href="https://buildagil.com" color="#006666">buildagil</Link> und Open Source für <Link href="https://stack.swiss" color="#f13608">STACK</Link>.  Zudem absolviere ich meinen MSc an der <Link href="https://zhaw.ch" color="#0064a6">ZHAW</Link>.
             </span>
         </div>
     </div>
