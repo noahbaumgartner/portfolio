a3ed8eb618aa8080f83e171c42752f6288d324c6
noah <baumgartner.noah@outlook.com>
2025-12-19 20:18:11 +0100

    changed nav to fixed nav, added backdrop blur

--- src/lib/components/sections/HeroSection.svelte
+++ src/lib/components/sections/HeroSection.svelte
@@ -48,7 +48,7 @@
 </script> 
 
 <div class="border-b border-neutral-500 px-4 sm:px-10 lg:px-16">
-    <div class="py-4 mx-auto max-w-7xl border-l border-r px-6 border-neutral-500 min-h-96 flex items-center justify-center grid-background relative">
+    <div class="pt-15 mx-auto max-w-7xl border-l border-r px-6 border-neutral-500 min-h-100 flex items-center justify-center grid-background relative">
         <ComponentName name="HeroSection.svelte" position="bottom-left" />
         <div class="flex flex-col space-y-2 items-center">
             <span class="text-left text-2xl font-[Google_Sans] p-2 rounded-sm flex space-x-1">
--- src/lib/components/sections/TitleSection.svelte
+++ src/lib/components/sections/TitleSection.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="border-b border-neutral-500 px-4 sm:px-10 lg:px-16">
-    <div class="py-4 mx-auto max-w-7xl border-l border-r px-16 border-neutral-500 min-h-40 flex items-center grid-background relative">
+    <div class="pt-15 mx-auto max-w-7xl border-l border-r px-16 border-neutral-500 min-h-48 flex items-center grid-background relative">
         <ComponentName name="TitleSection.svelte" position="bottom-right" />
         <div class="flex flex-col space-y-2 w-full text-center sm:text-left">
             <Heading1>{title}</Heading1>
