e3da68cf3f062dd0fd23dad419c36535345d04a9
noah <baumgartner.noah@outlook.com>
2025-12-19 18:16:05 +0100

    worked on hero typewriter effect

--- src/lib/components/sections/HeroSection.svelte
+++ src/lib/components/sections/HeroSection.svelte
@@ -37,16 +37,23 @@
 
         type();
     }
-</script>
+
+    function animateBlinking(node: HTMLElement) {
+        let visible = true;
+        setInterval(() => {
+            node.style.opacity = visible ? '1' : '0';
+            visible = !visible;
+        }, 300);
+    }
+</script> 
 
 <div class="border-b border-neutral-300 px-4 sm:px-10 lg:px-16">
     <div class="py-4 mx-auto max-w-7xl border-l border-r px-6 border-neutral-300 min-h-96 flex items-center justify-center grid-background relative">
         <ComponentName name="HeroSection.svelte" position="bottom-left" />
-        <div class="flex flex-col space-y-4 items-center">
-            <span class="text-left bg-neutral-200 text-2xl font-semibold font-[Google_Sans] p-2 rounded-sm flex">
-                <span>[</span>
-                <h1 {@attach animateGreeting} class="text-neutral-800">...</h1>
-                <span>]</span>
+        <div class="flex flex-col space-y-2 items-center">
+            <span class="text-left text-2xl font-[Google_Sans] p-2 rounded-sm flex space-x-1">
+                <h1 {@attach animateGreeting} class="font-semibold">...</h1>
+                <span {@attach animateBlinking} class="font-extralight">_</span>
             </span>
             <span class="text-center text-lg max-w-xl">
                 Ich entwickle Software bei <Link href="https://buildagil.com" color="#006666">buildagil</Link> und Open Source für <Link href="https://stack.swiss" color="#f13608">STACK</Link>.  Zudem absolviere ich meinen MSc an der <Link href="https://zhaw.ch" color="#0064a6">ZHAW</Link>.
