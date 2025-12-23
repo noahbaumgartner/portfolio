6ea6ec26a44d91e0feffd28d33f3eb1a7ef1d9be
noah <baumgartner.noah@outlook.com>
2025-12-23 18:36:21 +0100

    smaller ui fix for project preview

--- src/lib/components/ProjectPreview.svelte
+++ src/lib/components/ProjectPreview.svelte
@@ -9,9 +9,9 @@
 <a href={`/projects/${project.slug}`} data-sveltekit-preload-data>
     <div class="group border-b border-gray-500 flex flex-col md:flex-row cursor-pointer">
         <div class="p-3 flex items-center justify-center">
-            <img src={project.image} alt={project.title} class="aspect-[1.531/1] w-full md:min-w-66 md:w-66 object-contain grayscale group-hover:grayscale-0 duration-200" />
+            <img src={project.image} alt={project.title} class="aspect-[1.531/1] w-full md:min-w-66 md:w-66 object-contain grayscale group-hover:grayscale-0 transition-[filter] duration-300" />
         </div>
-        <div class="flex flex-col space-y-1 justify-center px-10 pb-10 pt-1 md:py-0 md:pl-5 md:pr-10">
+        <div class="flex flex-col space-y-1 justify-center px-10 pb-10 pt-2 md:py-0 md:pl-5 md:pr-10">
             <div class="flex space-x-2">
                 <Heading2>{project.title}</Heading2>
                 <ArrowUpRight class="size-6 group-hover:-translate-y-1 group-hover:translate-x-1 duration-150"/>
