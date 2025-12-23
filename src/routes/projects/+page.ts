7111f64fe7ad06e27cb800007730a020607c5ec9
noah <baumgartner.noah@outlook.com>
2025-12-23 22:05:53 +0100

    fixed projects list

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -3,17 +3,18 @@ import { error } from '@sveltejs/kit'
 
 export const load = async () => {
     try {
-        const files = await import.meta.glob('../content/projects/*', { eager: true });
+        const files = await import.meta.glob('../../content/projects/*', { eager: true });
         const projects = Object.entries(files).map(([path, module]) => {
             const { metadata } = module as { metadata: any };
 
-            const slug = path.split('/')[3].split('.')[0];
+            const slug = path.split('/')[4].split('.')[0];
             const project: ProjectDTO = {
                 slug,
                 title: metadata.title,
                 description: metadata.description,
                 image: `/images/projects/${slug}.webp`,
                 period: `${new Date(metadata.year, metadata.month - 1).toLocaleString('default', { month: 'long' })} ${metadata.year}`,
+                url: metadata.url,
                 tags: metadata.tags.split(",") || [],
             }
 
