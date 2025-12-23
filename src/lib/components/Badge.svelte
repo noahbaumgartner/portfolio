379d031186082fba9aa8dae33dd8602dfaba5a4f
noah <baumgartner.noah@outlook.com>
2025-12-23 21:21:56 +0100

    open path link in a new tab

--- src/lib/components/Badge.svelte
+++ src/lib/components/Badge.svelte
@@ -8,7 +8,7 @@
 <svelte:element
     this={href ? 'a' : 'span'}
     class={["px-2 py-0.5 text-xs bg-neutral-100 border border-neutral-200 rounded-full inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden [&>svg]:pointer-events-none [&>svg]:size-3", href ? "cursor-pointer": ""]}
-    {...(href ? { href } : {})}
+    {...(href ? { href, target: "_blank" } : {})}
 >
     {@render children?.()}
 </svelte:element>
