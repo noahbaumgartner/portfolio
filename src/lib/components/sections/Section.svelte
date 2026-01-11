72eb987f234a08a65d117f8cccf83439760709e0
noah <baumgartner.noah@outlook.com>
2026-01-11 16:12:42 +0100

    removed bottom border on copyright section component

--- src/lib/components/sections/Section.svelte
+++ src/lib/components/sections/Section.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import SectionNumber from "./SectionNumber.svelte";
 
-    let props: { sectionNumber?: string; class?: string; children?: any, variant?: "light" | "dark" } = $props();
+    let props: { sectionNumber?: string; class?: string; children?: any, variant?: "light" | "dark", noBottomBorder?: boolean } = $props();
 </script>
 
-<section class="border-b border-neutral-500 px-4 sm:px-10 lg:px-16">
+<section class="{props.noBottomBorder ? '' : 'border-b border-neutral-500'} px-4 sm:px-10 lg:px-16">
     <div class={["mx-auto lg:max-w-7xl border-l border-r border-neutral-500 flex items-center justify-center relative", props.class, props.variant === "dark" ? "bg-linear-150 from-neutral-950 from-50% via-neutral-900 via-70% to-neutral-950 to-90%" : ""]}>
         {#if props?.sectionNumber}
             <SectionNumber number={props.sectionNumber} />
