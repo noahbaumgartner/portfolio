c14659693f5c94b1de169ad1d20037e3a75e7321
noah <baumgartner.noah@outlook.com>
2026-01-11 16:17:35 +0100

    changed border color

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -7,13 +7,13 @@
 </script>
 
 <ProjectTitleSection project={project} />
-<div class="border-b border-neutral-500 px-4 sm:px-10 lg:px-16">
-    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-500 min-h-64 relative flex flex-col lg:flex-row">
+<div class="border-b border-neutral-300 px-4 sm:px-10 lg:px-16">
+    <div class="mx-auto lg:max-w-7xl border-l border-r border-neutral-300 min-h-64 relative flex flex-col lg:flex-row">
         <SectionNumber number="01" />
         <div class="grow p-10">
-            <Content class="border-b border-gray-500" />
+            <Content class="border-b border-neutral-300" />
         </div>
-        <div class="p-3 border-t lg:border-t-0 lg:border-l border-neutral-500 items-center justify-center flex">
+        <div class="p-3 border-t lg:border-t-0 lg:border-l border-neutral-300 items-center justify-center flex">
             <img src={project.image} alt={project.title} class="aspect-[1.531/1] w-full md:min-w-md md:w-md" />
         </div>
     </div>
